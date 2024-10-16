ab9ed8e3792308ca4e94aed6e4e3b6318371c351
diyajojo <diyaj2122@gmail.com>
2024-10-17 00:53:33 +0530

    made landing pg mobile repsosive

--- src/app/page.js
+++ src/app/page.js
@@ -17,39 +17,44 @@ const HomePage = () => {
     };
 
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-white" style={{ color: '#0F6464' }}>
+        <div className="flex flex-col items-center justify-center min-h-screen bg-white pt-20 sm: " style={{ color: '#0F6464' }}>
 
-            <h1 className="text-5xl font-bold mb-6">Welcome to the QR Treasure Hunt!</h1>
+<h1 className="text-4xl font-bold md:text-5xl  mb-6 ">
+    <span className='text-center
+    block md:inline'>Welcome to the </span> 
+    <span className='block md:inline'>QR Treasure Hunt!</span>
 
-            
-            <Image 
-                    src={ellipse} 
-                    alt="ellipse" 
-                    className="w-60 mb-10" 
-                    width={140} // Set the width
-                    height={126} // Set the height
-                    style={{position:'absolute', top:'31.5%', left:'0'}}
-                />
-            <p className="text-xl font-weight:500 font-medium mb-4" style={{color:'rgba(4,121,121,1)'}}>Unravel mysteries, scan QR codes, and top the leaderboard!</p>
+</h1>
+
+<div className="absolute left-[-45%] top-1.75 md:left-0 md:top-[31.5%]">
+    <Image 
+        src={ellipse} 
+        alt="ellipse" 
+        className="w-60 mb-10" 
+        width={140} 
+        height={126} 
+    />
+</div>
+            <p className="text-xl font-weight:500 font-medium mb-4" style={{color:'rgba(4,121,121,1)'}}>
+                <span className='block md:inline'>Unravel mysteries, scan QR codes,</span>
+                <span className=' text-center block md:inline'> and  top the leaderboard!
+                </span>
+                </p>
             
             <div style={{marginLeft:'-250px'}}>
-
+            
+            
+            <Image src={cubo} alt="cubo"
+            width={50} height={50} className='left-0 top-0 md:hidden'></Image>
             <button 
                     onClick={handleStartNow} 
-                    className="hover:bg-green-700 text-white  py-2 px-14 shadow-lg transition duration-300 ease-in-out transform hover:scale-105" 
+                    className="hover:bg-green-700 text-white  py-2 px-14 shadow-lg transition duration-300 ease-in-out transform hover:scale-105 -mt-4 ml-9 md:mt-10" 
                     style={{ backgroundColor: '#0C8B8B', boxShadow: `-8px 8px 0px rgba(11, 135, 135, 0.8)`,  borderRadius: '12px', position:'absolute' }}
                 >
                     Join the Adventure
                 </button>
 
-                <Image 
-                    src={cubo} 
-                    alt="cubo" 
-                    className="w-20 mb-4" 
-                    width={140} // Set the width
-                    height={126} // Set the height
-                    style={{position:'absolute', top:'31.5%', left:'39.5%'}}
-                />
+               
 
             </div>
             
@@ -57,15 +62,14 @@ const HomePage = () => {
                 <Image 
                     src={landingImg} 
                     alt="Treasure Hunt" 
-                    className="w-68 mb-8" 
-                    width={500} // Set the width
-                    height={750} // Set the height
+                    className="w-68 mb-8 mt-40 md:mt-0 w-90 mt-30 " 
+                    
                 />
             </div>
             <Image 
                     src={circle} 
                     alt="circle" 
-                    className="w-60 mb-8" 
+                    className="w-20  md:w-60 mb-8" 
                     style={{position:'absolute', top:'49.5%', right:'0'}}
                 />
         </div>
